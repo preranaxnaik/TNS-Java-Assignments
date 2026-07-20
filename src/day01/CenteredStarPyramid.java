28702e08cbd780b05c43aa56b46a2c08b4af6735
preranaxnaik <preranaxnaik@gmail.com>
2026-07-21 00:16:27 +0530

    Completed Day 01 pattern assignment

---
+++ src/day01/CenteredStarPyramid.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class CenteredStarPyramid {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ src/day01/ReverseDecreasingNumberTriangle.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class ReverseDecreasingNumberTriangle {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
