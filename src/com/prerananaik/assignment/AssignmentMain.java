0c477f031bee8fbbb55d5e6ebbc5c9ae0f285b75
preranaxnaik <preranaxnaik@gmail.com>
2026-07-22 00:01:21 +0530

    initialized second assignment

---
+++ src/com/prerananaik/assignment/AssignmentMain.java
@@ -0,0 +1,10 @@
+package com.prerananaik.assignment;
+
+public class AssignmentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/prerananaik/assignment/employees/Employee.java
@@ -0,0 +1,10 @@
+package com.prerananaik.assignment.employees;
+
+public class Employee {
+	public static void main(String[] args) {
+		String name = "Prerana";
+		int employeeId = 1001;
+		float salary = 56000.50f;
+		System.out.println("");
+	}
+}
