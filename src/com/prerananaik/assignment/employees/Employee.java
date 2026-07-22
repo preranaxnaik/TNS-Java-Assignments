a5d8e0648da5d5fa403d0b58e05f8e916985bb34
preranaxnaik <preranaxnaik@gmail.com>
2026-07-23 00:27:48 +0530

    Completed Java packages, inheritance, and access modifiers assignment

--- src/com/prerananaik/assignment/employees/Employee.java
+++ src/com/prerananaik/assignment/employees/Employee.java
@@ -1,10 +1,67 @@
 package com.prerananaik.assignment.employees;
 
 public class Employee {
-	public static void main(String[] args) {
-		String name = "Prerana";
-		int employeeId = 1001;
-		float salary = 56000.50f;
-		System.out.println("");
+    // Private members
+    private String name;
+    private int employeeId;
+    private float salary;
+
+    // Protected member
+    protected String companyName;
+
+    // Default access method
+    void displayCompany() {
+        System.out.println("Company Name : " + companyName);
+    }
+
+    // Default constructor
+    public Employee() {
+
+    }
+
+    // Parameterized constructor
+    public Employee(String name, int employeeId, float salary, String companyName) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+        this.companyName = companyName;
+    }
+
+    // Getters & Setters
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+
+	public float getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+
 }
--- src/com/prerananaik/assignment/employees/Manager.java
+++ src/com/prerananaik/assignment/employees/Manager.java
@@ -1,10 +1,29 @@
 package com.prerananaik.assignment.employees;
 
 public class Manager extends Employee{
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stubs
-
+	int teamSize;
+	
+	public int getTeamSize() {
+		return teamSize;
 	}
+	
+	public void setTeamSize(int teamSize) {
+		this.teamSize = teamSize;
+	}
+	
+    public Manager() {
+
+    }
+
+    public Manager(String name, int employeeId, float salary,String companyName, int teamSize)
+    {
+        super(name, employeeId, salary, companyName);
+        this.teamSize = teamSize;
+    }
+
+    public void showCompany() {
+        System.out.println("Company : " + companyName);
+    }
+
 
 }
