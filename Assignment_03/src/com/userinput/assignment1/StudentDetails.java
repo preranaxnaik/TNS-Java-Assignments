453c1fa21c39762ddb4d7b471a8a5fa477472f6c
preranaxnaik <prerana91042@gmail.com>
2026-08-07 12:02:19 +0530

    implemented user defined input in java

--- Assignment_03/src/com/userinput/assignment1/StudentDetails.java
+++ Assignment_03/src/com/userinput/assignment1/StudentDetails.java
@@ -6,10 +6,12 @@ public class StudentDetails {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("-------Please Enter your Details [name. rollno, grade , percenatge]-------");
 		String fullName = sc.nextLine();
 		int rollNo = sc.nextInt();
 		char grade = sc.next().charAt(0);
 		float percentage = sc.nextFloat();
+		
 		
 		System.out.println(fullName);
 		System.out.println(rollNo);
