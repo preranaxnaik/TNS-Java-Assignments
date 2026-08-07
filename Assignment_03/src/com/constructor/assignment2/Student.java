885ba58925a539873c623f3a69931ff2e03269b5
preranaxnaik <prerana91042@gmail.com>
2026-08-07 20:55:44 +0530

    implemented default constructor in java

---
+++ Assignment_03/src/com/constructor/assignment2/Student.java
@@ -0,0 +1,13 @@
+package com.constructor.assignment2;
+
+public class Student {
+	
+	public Student() {
+		System.out.println("Student object is created.");
+	}
+	
+	public static void main(String[] args) {
+		Student obj = new Student();
+	}
+
+}
